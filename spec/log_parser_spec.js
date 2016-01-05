4d05cb65349a7d7b0598cb84ddb809d1a50d8e9e
Joshua Sparks <JoshuaTaylorBW@gmail.com>
2016-01-05 11:05:05 -0700

    finished

--- spec/log_parser_spec.js
+++ spec/log_parser_spec.js
@@ -19,13 +19,13 @@ describe('Parses dates from logs', function () {
     expect(parser.prepLogs(logs)).toEqual(expected);
   })
 
-  xit('extracts array of unique log dates', function () {
+  it('extracts array of unique log dates', function () {
     var expected = [ '2014-05-10', '2014-05-11', '2014-05-12' ]
 
     expect(parser.getDates(logs)).toEqual(expected);
   })
 
-  xit('prints the list of log dates', function () {
+  it('prints the list of log dates', function () {
     var dates = [ '2014-05-10', '2014-05-11', '2014-05-12' ]
     var expected = '* What are all the dates the log covers?\n' +
     '2014-05-10\n' + '2014-05-11\n' + '2014-05-12\n';
